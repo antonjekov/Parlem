7df8110089d1cb59a5dc042a5b4978035c7ad0ae
Anton Jekov <antonjekov@gmail.com>
2022-03-03 11:31:14 +0100

    Initial commit

---
+++ Parlem.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Parlem.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "Parlem";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ Services/Parlem.Services.Data/SettingsService.cs
@@ -0,0 +1,29 @@
+﻿namespace Parlem.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Parlem.Data.Common.Repositories;
+    using Parlem.Data.Models;
+    using Parlem.Services.Mapping;
+
+    public class SettingsService : ISettingsService
+    {
+        private readonly IDeletableEntityRepository<Setting> settingsRepository;
+
+        public SettingsService(IDeletableEntityRepository<Setting> settingsRepository)
+        {
+            this.settingsRepository = settingsRepository;
+        }
+
+        public int GetCount()
+        {
+            return this.settingsRepository.AllAsNoTracking().Count();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            return this.settingsRepository.All().To<T>().ToList();
+        }
+    }
+}
