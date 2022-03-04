0c513ec2d74416e280e97e62473c109227744d0a
Anton Jekov <antonjekov@gmail.com>
2022-03-04 18:20:50 +0100

    Angular App added interfaces

---
+++ Web/Parlem.Web.AngularIonic/Parlem/src/app/home/client.ts
@@ -0,0 +1,10 @@
+export interface Client{
+  _id: string;
+  docType: string;
+  docNum: string;
+  email: string;
+  customerId: string;
+  givenName: string;
+  familyName1: string;
+  phone: string;
+}
--- Web/Parlem.Web.AngularIonic/Parlem/src/app/home/home.page.ts
+++ Web/Parlem.Web.AngularIonic/Parlem/src/app/home/home.page.ts
@@ -1,19 +1,25 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Client } from './client';
+import { Product } from './product';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit, OnDestroy{
 
   form: FormGroup;
-  products: any;
-  client: any;
+  products: Product[];
+  client: Client;
+  prodSub: Subscription;
+  clientSub: Subscription;
 
   constructor(private http: HttpClient) {}
+
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -24,15 +30,22 @@ export class HomePage implements OnInit{
     });
   }
 
+  ngOnDestroy(): void {
+    this.clientSub.unsubscribe();
+    this.prodSub.unsubscribe();
+  }
+
   onDocNum(){
     if (!this.form.valid) {
       return;
     }
-    this.http
+
+    this.prodSub = this.http
     .get(`https://localhost:44319/api/products?docNum=${this.form.value.docNum}`)
-    .subscribe(x=>this.products=x);
-    this.http
+    .subscribe(x=>this.products=x as Product[]);
+
+    this.clientSub = this.http
     .get(`https://localhost:44319/api/clients?docNum=${this.form.value.docNum}`)
-    .subscribe(x=>this.client=x);
+    .subscribe(x=>this.client=x as Client);
   }
 }
